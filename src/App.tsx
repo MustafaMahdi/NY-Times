278cb821e0f4a8c81b79c5944d967301abc698dc
MMahdi19 <129668865+MMahdi19@users.noreply.github.com>
2024-07-24 12:40:24 +0300

    add. main route

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Redirect, Route, Switch } from "react-router-dom";
+import "./App.css";
+import { ROUTES } from "./constants";
+import { Articles } from "./pages";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Switch>
+            <Route path={ROUTES.HOME} exact>
+                <Redirect to={ROUTES.ARTICLES} />
+            </Route>
+            <Route path={ROUTES.ARTICLES} exact>
+                <Articles />
+            </Route>
+        </Switch>
+    );
 }
 
 export default App;
---
+++ src/constants/routes.ts
@@ -0,0 +1,4 @@
+export const ROUTES = {
+    HOME: "/",
+    ARTICLES: "/articles",
+};
