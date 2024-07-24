08a59607111337a5b465fba6b1923584e4f40527
MMahdi19 <129668865+MMahdi19@users.noreply.github.com>
2024-07-24 16:15:20 +0300

    Handle home click

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,29 @@
-import { Stack, Typography } from "@mui/material";
-import { Redirect, Route, Switch } from "react-router-dom";
+import { Link, Stack } from "@mui/material";
+import { Redirect, Route, Switch, useHistory } from "react-router-dom";
 import "./App.css";
 import { ROUTES } from "./constants";
 import { ArticleDetails, Articles } from "./pages";
 import ProviderWrapper from "./providers/ProviderWrapper";
 
 function App() {
+    const navigate = useHistory();
+    const onHomeClickHandler = () => {
+        navigate.push(`${ROUTES.ARTICLES}`);
+    };
     return (
         <ProviderWrapper>
             <Stack padding={5}>
                 <Stack gap={2}>
-                    <Typography variant="h3">NY Times</Typography>
+                    <Link
+                        align="left"
+                        underline="none"
+                        color={"text.primary"}
+                        component="button"
+                        variant="h3"
+                        onClick={onHomeClickHandler}
+                    >
+                        NY Times
+                    </Link>
                     <Switch>
                         <Route path={ROUTES.HOME} exact>
                             <Redirect to={ROUTES.ARTICLES} />
